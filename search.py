564aa3a0f3b6f1711d8048658fe0fef30697fb75
linitachi <linitachi@gmail.com>
2019-10-22 12:37:54 +0800

    refactor

--- search.py
+++ search.py
@@ -1,6 +1,7 @@
 from crawler import PTTCrawler
 import urllib
 import requests
+import time
 
 
 class Search(PTTCrawler):
@@ -8,21 +9,32 @@ class Search(PTTCrawler):
     def __init__(self):
         pass
 
-    def search_for_keyword(self, url, keyword='sony'):
+    def requestResp(self, url, keyword, page):
+        search_endpoint_url = url + '/search'
+        resp = requests.get(search_endpoint_url, params={
+                            'q': keyword, 'page': page})
+        return resp
+
+    def search_for_keyword(self, url, keyword='sony', page=1):
         '''
         :param url 個版的網址
         :param keyword 想搜尋的文字
         '''
-        search_endpoint_url = url + '/search'
-        resp = requests.get(search_endpoint_url, params={'q': keyword})
-        return self.transferto_Metadata(resp)
+        return self.transferto_Metadata(self.requestResp(url, keyword, page))
 
     def search_for_author(self, url, keyword=''):
         '''
         :param url 個版的網址
         :param keyword 想搜尋的作者
         '''
-        search_endpoint_url = url + '/search'
-        resp = requests.get(search_endpoint_url, params={
-                            'q': 'author:'+keyword})
-        return self.transferto_Metadata(resp)
+        keyword = 'author:'+keyword
+        return self.transferto_Metadata(self.requestResp(url, keyword, page))
+
+    # def search_for_date(self, url, keyword=''):
+    #     '''
+    #     :param url 個版的網址
+    #     :param keyword 想搜尋的時間
+    #     '''
+    #     if keyword == '':
+    #         localtime = time.localtime(time.time())
+    #         keyword = str(localtime.tm_mon) + '/' + str(localtime.tm_mday)
