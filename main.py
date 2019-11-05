7040b2c7a1148e9a8f25809559d224d8bec9ec8b
linitachi <linitachi@gmail.com>
2019-11-05 21:55:36 +0800

    refactor

--- main.py
+++ main.py
@@ -6,14 +6,17 @@ import crawler_process
 if __name__ == '__main__':
     pages = 2
     csvTitle = 'index.csv'
+    keyword = 'u11'
+    find_all = 0
     start = time.time()
     Processor = crawler_process.CrawlerProcessor(csvTitle)
 
-    # 如果要存全部檔案
-    # start_url = 'https://www.ptt.cc/bbs/mobilesales/index.html'
-    # Processor.saveAll(start_url, pages)
-
-    # 如果要存特定關鍵字檔案
-    start_url = 'https://www.ptt.cc/bbs/mobilesales/'
-    Processor.saveKeyword(start_url, pages, 'u11')
-    print('花費: %f 秒' % (time.time() - start))
+    if find_all:
+        # 如果要存全部檔案
+        start_url = 'https://www.ptt.cc/bbs/mobilesales/index.html'
+        Processor.saveAll(start_url, pages)
+    else:
+        # 如果要存特定關鍵字檔案
+        start_url = 'https://www.ptt.cc/bbs/mobilesales/'
+        Processor.saveKeyword(start_url, pages, keyword)
+        print('花費: %f 秒' % (time.time() - start))
